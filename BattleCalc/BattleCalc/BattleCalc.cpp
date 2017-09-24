b8f9eb719ce8fb72352633465089d19d806aa70c
Maria Maltseva <gangan_m@rambler.ru>
2017-09-24 12:02:12 +0300

    Add simple profile test

--- BattleCalc/BattleCalc/BattleCalc.cpp
+++ BattleCalc/BattleCalc/BattleCalc.cpp
@@ -1,8 +1,34 @@
 #include "stdafx.h"
+#include "ModelProfile.h"
+#include "Model.h"
+
+#include <vector>
+
+void dump( const Model& m )
+{
+	printf( "\n Model: %s", m.toString().c_str() );
+}
+
+void TestProfileChoice()
+{
+	Profile p1( 5 );
+	Profile p2( 1 );
+	Profile p3( 3 );
+	Profile p4( 10 );
+
+	std::vector<Profile> profiles = { p1, p2, p3, p4 };
+	Model m( profiles );
+
+	dump( m );
+}
 
 
 int main()
 {
-    return 0;
+	TestProfileChoice();
+
+	printf( "\n\nEnd" );
+	getchar();
+	return 0;
 }
 
